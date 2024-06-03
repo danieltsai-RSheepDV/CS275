fb9099a58a503f76e86fd7f71d6cd57d04c531f2
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2024-06-02 18:42:20 -0700

    muscled joints

---
+++ Assets/Scripts/MuscledJoint.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuscledJoint : MonoBehaviour
+{
+    [SerializeField] private int numMuscles;
+    [SerializeField] private float neutralK;
+    private SpringJoint[] muscles;
+    private float[] muscleActivations;
+    public float this[int key]{
+        get => muscleActivations[key];
+        set
+        {
+            muscleActivations[key] = value;
+            muscles[key].spring = value * neutralK * 2;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpringJoint[] allSprings = GetComponents<SpringJoint>();
+        List<SpringJoint> muscleJoints = new List<SpringJoint>();
+        for (int i = numMuscles; i < allSprings.Length; i++)
+        {
+            muscleJoints.Add(allSprings[i]);
+        }
+        muscles = muscleJoints.ToArray();
+
+        muscleActivations = new float[numMuscles];
+    }
+}
